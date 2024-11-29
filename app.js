eea1f9a93f5ffcffe1a9d711ab03aa964586e7f3
youness bachar khir <81125054+younessbachar@users.noreply.github.com>
2024-11-29 02:29:04 +0100

    Update app.js

--- app.js
+++ app.js
@@ -95,7 +95,7 @@ app.post("/user/add.html", (req, res) => {
  
       Customer.create(req.body)
       .then(() => {
-        res.redirect("/user/add.html");
+        res.redirect("/");
       })
       .catch((err) => {
         console.log(err);
